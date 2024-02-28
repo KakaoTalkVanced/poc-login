b876cbdba4ca7d9f0ca8039e1ed4baa2e09bab99
ck <computer_king@naver.com>
2024-02-29 02:11:12 +0900

    edit MainWindow
    add basic rest api for test post method

--- login/MainWindow.xaml.cs
+++ login/MainWindow.xaml.cs
@@ -1,28 +1,31 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Net.Http;
+using System.Net.Http.Json;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
-namespace login
+namespace login;
+
+public partial class MainWindow : Window
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
-    public partial class MainWindow : Window
+    readonly string _baseUrl = "https://katalk.kakao.com";
+
+    public MainWindow()
     {
-        public MainWindow()
-        {
-            InitializeComponent();
-        }
+        InitializeComponent();
+    }
+
+    void Button_Click(object sender, RoutedEventArgs e)
+    {
+        Request();
+    }
+
+    void Request()
+    {
+        using var client = new HttpClient();
+
+        var requestTask = client.PostAsJsonAsync(_baseUrl, string.Empty);
+
+        var httpResponseMessage = requestTask.Result;
+
+        ResultTextBlock.Text = httpResponseMessage.ToString();
     }
 }
